05f6b796057b23c20f6871b3aea44ca6036b6cab
Kunalll1 <kunalkumarjha97@gmail.com>
2024-05-01 19:20:23 +0530

    added del button to the ui

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 function Card({ massage }) {
   return (
-    <div className="flex col items-center justify-around py-4 px-5 bg-gray-600 rounded-lg shadow-md text-white">
+    <div className="flex col items-center justify-around py-4 px-5 bg-gray-600 rounded-lg shadow-md text-white gap-4">
       <h2 className="text-2xl capitalize mr-12 font-semibold">{massage}</h2>
       <div className="flex gap-2">
         <Button
@@ -28,6 +28,13 @@ function Card({ massage }) {
           }
           css={"bg-red-700"}
           text={"0"}
+        />
+      </div>
+      <div className="border-l-2 border-[#f8fafc42] ">
+        <Button
+          css={"bg-black ml-2"}
+          icon={<CiTrash size={"4vh"} />}
+          text={"."}
         />
       </div>
     </div>
